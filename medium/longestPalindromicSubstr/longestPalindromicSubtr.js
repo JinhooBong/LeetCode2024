420e4a6997349efa9acaa005720039ae45836ac3
Jinhoo Bong <jinhoobong@gmail.com>
2024-07-17 11:48:52 -0700

    daily medium problem solution

--- medium/longestPalindromicSubstr/longestPalindromicSubtr.js
+++ medium/longestPalindromicSubstr/longestPalindromicSubtr.js
@@ -19,5 +19,26 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
+    let bestStart = 0;
+    let bestEnd = 0;
     
+    const performMiddleOut = (start, end) => {
+        while (start >= 0 && end < s.length && s[start] === s[end]) {
+            start--;
+            end++;
+        }
+        start++;
+        end--;
+        if (end - start + 1 > bestEnd - bestStart + 1) {
+            bestStart = start;
+            bestEnd = end;
+        }
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        performMiddleOut(i, i)
+        performMiddleOut(i, i + 1)
+    }
+
+    return s.slice(bestStart, bestEnd + 1);
 };
