dcd225f1060e46a602b2526d177c4934c8e9dc6f
Jinhoo Bong <jinhoobong@gmail.com>
2024-07-17 11:24:03 -0700

    daily easy problem solution

--- easy/romanToInt/romanToInt.js
+++ easy/romanToInt/romanToInt.js
@@ -37,5 +37,65 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    // input: a string s
+    // output: the integer value of the string s
+
+    // create a global sum variable to keep track of the value
+    let sum = 0;
+
+    const romanValues = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    // if a smaller value precedes a larger value, it usually means it's substracting 
+    // so as we iterate through the string s, 
+    // we check to see if the next string is larger, if so, we add the negative value of it, 
+    // else we add the value 
+
+    for (let i = 0; i < s.length; i++) {
+        if (i + 1 < s.length && romanValues[s[i]] < romanValues[s[i+1]]) {
+            // sum += negative of s[i]
+            sum -= romanValues[s[i]]
+        } else {
+            sum += romanValues[s[i]]
+        }
+    }
+
+    return sum;
+};
+
+
+// Solution 2 
+var romanToInt = function(s) {
+	const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = sym[s[i]];
+        const next = sym[s[i + 1]];
+
+        if (cur < next) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+    }
+
+    return result;
 };
