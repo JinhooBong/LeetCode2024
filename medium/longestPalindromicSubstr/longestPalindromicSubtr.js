20c165b28467544ee65f0dde980aec99e28fc842
Jinhoo Bong <jinhoobong@gmail.com>
2024-07-17 11:04:55 -0700

    medium problem setup

---
+++ medium/longestPalindromicSubstr/longestPalindromicSubtr.js
@@ -0,0 +1,23 @@
+/**
+ * Given a string s, return the longest palindromic substring in s
+ * 
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * 
+ * "aba" is also valid
+ * 
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ */
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+};
